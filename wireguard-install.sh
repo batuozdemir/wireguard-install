d16d0d7c8c185edbbec9b048c67c7ece60dfed97
batuozdemir <batuozdemir@gmail.com>
2024-03-28 21:47:34 +0300

    edit

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -136,7 +136,7 @@ function installQuestions() {
 	done
 
 	# Generate random number within private ports range
-	RANDOM_PORT=$(shuf -i49152-65535 -n1)
+	RANDOM_PORT=51820
 	until [[ ${SERVER_PORT} =~ ^[0-9]+$ ]] && [ "${SERVER_PORT}" -ge 1 ] && [ "${SERVER_PORT}" -le 65535 ]; do
 		read -rp "Server WireGuard port [1-65535]: " -e -i "${RANDOM_PORT}" SERVER_PORT
 	done
